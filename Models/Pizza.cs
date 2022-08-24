d51b8a39e826cd1832068f85488e77ffa1f40334
Franklin Rodriguez <frank14082013@gmail.com>
2022-08-23 23:36:41 -0600

    Se agregan "decoradores" Para especificar atributos necesarios

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -11,7 +11,9 @@ namespace ContosoPizza.Models
 
 
 		public int Id { get; set; }
+		[Required (ErrorMessage	="DEBE ESPECIFICARSE EL NOMBRE!")]
 		public string? Name { get; set; }
+		[Required (ErrorMessage	="DEBE ESPECIFICARSE SI ES LIBRE DE GLUTEN!")]
 		public bool IsGlutenFree { get; set; }
 
 
